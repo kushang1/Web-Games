ce450a80d07f74575c908210ba5473fe0866595d
unknown <panchalkushang949@gmail.com>
2024-07-13 21:42:39 +0530

    Simon Game

---
+++ game.js
@@ -0,0 +1,79 @@
+var gamePattern = []
+var buttonColours = ["red","blue","green","yellow"];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if (!started){
+        $("#level-title").text("Level " + level );
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+})
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.random();
+    randomNumber = randomNumber*4;
+    randomNumber = Math.floor(randomNumber);
+    // console.log(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    // console.log(randomChosenColour);
+    gamePattern.push(randomChosenColour);
+    //  console.log(gamePattern);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    },100)
+}
+
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        // console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
